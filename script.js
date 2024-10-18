7b99220cfd9f7d9ad2f04a8d6542218c3e6db7a7
AbdullahAlKhafajiDev <abdullahalkafajy321@gmail.com>
2024-10-18 04:38:57 -0500

    Setup the file/boiler-plate structure.

---
+++ script.js
@@ -0,0 +1,3 @@
+// Figure out a way to load a WASM GO file, and use it to: 
+// Call the WASM GO file on the dropped file that needs to be sent.
+// On recieve key input, call the WASM GO file to recieve the file.
